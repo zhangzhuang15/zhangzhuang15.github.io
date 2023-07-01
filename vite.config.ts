b098807f12ce8f313a1804d8b21936e29fe14b75
zhangzhuang15 <2592004894@qq.com>
2023-07-01 22:20:40 +0800

    resolve the conflict between CommonJS and ESM when docs:build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  assetsInclude: ["**/*.mp3"]
+  assetsInclude: ["**/*.mp3"],
+  ssr: {
+    noExternal: ["naive-ui"],
+  },
+
 })
