cb139030f1ea877c3420df400b4a498a712cd05e
zhangzhuang15 <2592004894@qq.com>
2023-09-09 20:37:31 +0800

    add two blogs about tsconfig and pretty code snippets; change the position of some files; add eslint and prettier configs;

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,14 +1,14 @@
 import DefaultTheme from "vitepress/theme";
 import type { App } from "vue";
 import "./custom.css";
-import Giscus from "../../docs/components/Giscus.vue";
+import Giscus from "./components/Giscus.vue";
 
 export default {
-    extends: DefaultTheme,
+  extends: DefaultTheme,
 
-    // 在任何一个.md文件中，无需引入，直接使用Giscus组件
-    // refer: https://vitepress.dev/guide/extending-default-theme#registering-global-components
-    enhanceApp({ app }: { app: App }) {
-        app.component("Giscus", Giscus);
-    }
+  // 在任何一个.md文件中，无需引入，直接使用Giscus组件
+  // refer: https://vitepress.dev/guide/extending-default-theme#registering-global-components
+  enhanceApp({ app }: { app: App }) {
+    app.component("Giscus", Giscus);
+  },
 };
